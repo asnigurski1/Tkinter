52067d43d4388d49091bba9f8d574c69ff0cf6a6
asnigurski1 <75775512+asnigurski1@users.noreply.github.com>
2021-01-18 16:21:11 +0200

    Add files via upload

---
+++ turtle1.py
@@ -0,0 +1,48 @@
+import turtle
+
+#akna seaded
+aken = turtle.Screen()
+aken.setup(300,300)
+aken.title("Ülesane 1 - Artur Snigurski")
+
+k1 = turtle.Turtle()
+k1.color('orange')
+k1.lt(90)
+k1.fd(100)
+
+k2 = turtle.Turtle()
+k2.color('red')
+k2.lt(45)
+k2.fd(100)
+
+k3 = turtle.Turtle()
+k3.color('lightgreen')
+k3.fd(100)
+
+k4 = turtle.Turtle()
+k4.color('orange')
+k4.rt(45)
+k4.fd(100)
+
+k5 = turtle.Turtle()
+k5.color('red')
+k5.rt(90)
+k5.fd(100)
+
+
+k6 = turtle.Turtle()
+k6.color('lightgreen')
+k6.rt(135)
+k6.fd(100)
+
+k7 = turtle.Turtle()
+k7.color('blue')
+k7.rt(180)
+k7.fd(100)
+
+k8 = turtle.Turtle()
+k8.color('blue')
+k8.lt(135)
+k8.fd(100)
+
+turtle.exitonclick()
